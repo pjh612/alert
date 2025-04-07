696dbf5b438a11d0327829a022f90d9a57c84ec1
pjh612 <pjh_jn@naver.com>
2025-04-07 10:57:14 +0900

    jitpack gradle 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,33 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("java")
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.4"
+    id("maven-publish")
 }
 
 group = "com.alert"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
+tasks.named<Jar>("jar") {
+    archiveClassifier.set("") // plain 제거, 기본 jar 이름으로 설정
+}
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
-    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("net.gpedro.integrations.slack:slack-webhook:1.4.0")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -23,4 +36,15 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.pjh612"
+            artifactId = "alert"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
 }
